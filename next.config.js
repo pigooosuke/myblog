f8efc2cbcba72caabbfa2b09b39d376f80c21f5d
pigooosuke <hifirole@gmail.com>
2022-02-13 16:04:43 +0900

    update

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['s3.us-west-2.amazonaws.com', 'pigooosuke.com'],
+    domains: ['s3.us-west-2.amazonaws.com', 'image.pigooosuke.com'],
   },
 }
 
