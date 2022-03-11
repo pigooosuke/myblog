e795552f9f5f586981cd11a873a18f62630b720d
pigooosuke <hifirole@gmail.com>
2022-03-11 22:19:32 +0900

    add revalidate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,44 @@
-import type { ReactElement, ReactNode } from 'react'
-import { BlogListView } from '@/components/blogs/blogListView'
-import { getPosts } from '@/lib/notion/client'
-import { buildPost, collectList } from '@/lib/notion/client'
-import { QueryDatabaseResponseRecord, Post } from '@/types/blog'
-import generateRSSFeed from '@/lib/feed'
-import { LayoutMain } from '@/layout/main'
-import styles from '@/styles/index.module.css'
+import type { ReactElement } from "react";
+import { BlogListView } from "@/components/blogs/blogListView";
+import { getPosts } from "@/lib/notion/client";
+import { buildPost, collectList } from "@/lib/notion/client";
+import { QueryDatabaseResponseRecord, Post } from "@/types/blog";
+import generateRSSFeed from "@/lib/feed";
+import { LayoutMain } from "@/layout/main";
+import styles from "@/styles/index.module.css";
 
 export async function getStaticProps() {
-  const posts = await getPosts()
-  let post_lists = collectList(posts).map((block: QueryDatabaseResponseRecord) => buildPost(block))
+  const posts = await getPosts();
+  let post_lists = collectList(posts).map(
+    (block: QueryDatabaseResponseRecord) => buildPost(block)
+  );
   // generate rss feed
-  generateRSSFeed(post_lists)
+  generateRSSFeed(post_lists);
 
   return {
     props: {
-      post_lists
-    }
-  }
+      post_lists,
+    },
+    revalidate: 60,
+  };
 }
 
-const Home = ({ post_lists = [] }: { post_lists: QueryDatabaseResponseRecord[] }) => {
+const Home = ({
+  post_lists = [],
+}: {
+  post_lists: QueryDatabaseResponseRecord[];
+}) => {
   return (
     <>
       <div className={styles.contents}>
         {post_lists.map((post: Post) => {
-          return (<BlogListView post={post} key={post.slug} />)
+          return <BlogListView post={post} key={post.slug} />;
         })}
       </div>
     </>
-  )
-}
+  );
+};
 
-Home.getLayout = (page: ReactElement) => <LayoutMain>{page}</LayoutMain>
+Home.getLayout = (page: ReactElement) => <LayoutMain>{page}</LayoutMain>;
 
-export default Home
+export default Home;
